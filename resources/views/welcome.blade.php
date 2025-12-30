dc6101836174ff3450488749ab16f0b396b4bc20
Pedrito Martin <mpedrito98@gmail.com>
2025-12-30 19:43:58 -0400

    Descripción de los cambios realdtrdf ytfyt}izado quhfhghgvgfdfgis

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/js/app.js')
-    <title>Laravel 12 + Vue 3</title>
+ 
 </head>
 
 
 <body>
 
     <div id="app"></div>
+@vite('resources/js/app.js')
 </body>
 </html>
